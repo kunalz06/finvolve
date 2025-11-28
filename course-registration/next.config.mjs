10219430f97d1051f75ffef59ea91e2d00de91c8
kunalz06 <mitraricky06@gmail.com>
2025-11-28 19:10:42 +0530

    Add redirect from root to basePath in course-registration

--- course-registration/next.config.mjs
+++ course-registration/next.config.mjs
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   basePath: '/iemminorcourse',
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/iemminorcourse',
+        permanent: false,
+        basePath: false,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
